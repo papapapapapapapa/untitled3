2f58e459f61a9c324f6917afdeb9d534b3bc3d7a
papapapapapapapa <r3386588@gmail.com>
2017-10-15 02:29:03 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner in = new Scanner(System.in);
+        String str;
+        int a,b,c,d;
+        str=in.nextLine();
+        ///1234
+        a=((str.charAt(0)-'0')+7)%10;
+        b=((str.charAt(1)-'0')+7)%10;
+        c=((str.charAt(2)-'0')+7)%10;
+        d=((str.charAt(3)-'0')+7)%10;
+        //8901
+        //0189
+        System.out.printf("%d%d%d%d\n",c,d,a,b);
+
+    }
+}
